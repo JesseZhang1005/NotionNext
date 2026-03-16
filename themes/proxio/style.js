64560879f930d8b0332b55f4904d8e27278372db
JesseZhang1005 <mr.box1005@gmail.com>
2026-03-17 00:39:27 +0800

    Update style.js
    底部加入：/* 强制“我的项目经历”区块内的按钮向右对齐 */
        #testimonials .mt-8.w-full.flex.justify-start {
          justify-content: flex-end !important;
        }

--- themes/proxio/style.js
+++ themes/proxio/style.js
@@ -264,6 +264,11 @@ const Style = () => {
 #testimonials p {
   white-space: pre-wrap !important;
 }
+
+/* 强制“我的项目经历”区块内的按钮向右对齐 */
+    #testimonials .mt-8.w-full.flex.justify-start {
+      justify-content: flex-end !important;
+    }  
   `}</style>
 }
 
