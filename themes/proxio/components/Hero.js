c9e792387ac30306d2b3178e5676143cfbff1b70
JesseZhang1005 <mr.box1005@gmail.com>
2026-03-18 23:02:19 +0800

    Update Hero.js
    src={bannerIframe}

--- themes/proxio/components/Hero.js
+++ themes/proxio/components/Hero.js
@@ -41,7 +41,7 @@ export const Hero = props => {
           />
         )}
         <iframe
-          src='/star-background/index.html'
+          src={bannerIframe}
           className='w-full absolute h-screen left-0 top-0 pointer-events-none'
         />
         {/* 阴影遮罩 */}
