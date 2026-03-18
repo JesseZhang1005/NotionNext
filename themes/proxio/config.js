15a0122e891420535bcd54f6dd6a1c49a4effef7
JesseZhang1005 <mr.box1005@gmail.com>
2026-03-19 00:24:24 +0800

    Update config.js
    最新动态

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   PROXIO_BLOG_ENABLE: true, // 首页博文区块开关
   PROXIO_BLOG_TITLE: 'Updates',
   PROXIO_BLOG_COUNT: 4, // 首页博文区块展示前4篇文章
-  PROXIO_BLOG_TEXT_1: '动 态',
+  PROXIO_BLOG_TEXT_1: '最新动态',
 
   // 区块默认内容显示文章的summary文本，但也支持用自定义图片或logo德国替换掉占位显示内容
   PROXIO_BLOG_PLACEHOLDER_IMG_URL_1: '', // 填写要替换成的图片，支持图床或直接上传到项目中，示例  /images/feature-1.webp
